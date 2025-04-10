52369bb2102a881b7b661c07dfe717ecba319f79
Korrza <74359610+Korrza@users.noreply.github.com>
2025-04-10 02:20:24 +0200

    Feat: Add wav file + audio file reading

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("hello world")
+import wave
+import sys
+
+import pyaudio
+
+
+FILE_NAME = "nier"
+CHUNK = 1024
+
+with wave.open(f'assets/{FILE_NAME}.wav', 'rb') as wf:
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                    channels=wf.getnchannels(),
+                    rate=wf.getframerate(),
+                    output=True)
+
+    while len(data := wf.readframes(CHUNK)):
+        stream.write(data)
+
+    stream.close()
+    p.terminate()
