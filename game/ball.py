c48c3016f93ad1d42e2a47f5e597cbcbc49189ee
Shhevla <105705299+Shhevla@users.noreply.github.com>
2025-04-16 16:19:23 +0200

    Add ball
    Add ball where the ball bounces on ground and walls

---
+++ game/ball.py
@@ -0,0 +1,47 @@
+import pygame
+import random
+
+
+pygame.init()
+width, height = 800, 400
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Equalizer")
+clock = pygame.time.Clock()
+
+
+ball = pygame.Rect(400, 0, 10, 10)
+accely = 1
+vely = 3
+accelx = 0
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    vely += accely
+    ball.y += vely
+    ball.x += accelx
+    if ball.y >= height -10:
+        ball.y = height - 10
+        vely = -vely
+        accelx = random.randint(-20 , 20)
+
+    if ball.y <= 0:
+        ball.y =  0
+        vely = -vely
+        accelx = random.randint(-20 , 20)
+
+    if ball.x <= 0:
+        accelx = 20
+    
+    if ball.x >= width -10:
+        accelx = -20
+    
+    screen.fill((10, 10, 10))
+    pygame.draw.circle(screen, "White", ball.center, 5, 5)
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
